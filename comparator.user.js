e26b033c99a53c9111f2ad0d2c69e9a9e5b61db7
Deak Zsolt <lordmairtis@gmail.com>
2017-12-20 19:38:50 +0100

    adding and removing refs for compare

--- comparator.user.js
+++ comparator.user.js
@@ -14,40 +14,59 @@
     'use strict';
     let urlList = [];
     const SPINNER = `<i class="fa fa-spinner fa-spin fa-2x fa-fw" style="float: right; margin-top: 15px;"></i>`;
-    const GET_VALUE_BUTTON = $(`<i class="fa fa-magic fa-2x" style="float: right; margin-top: 15px;
-                            background-color: orange; border-radius: 18px; border: 7px;
-                            border-color: orange; border-style: solid; cursor: pointer;" aria-hidden="true"></i>`);
+    const GET_VALUE_BUTTON = $(`<div style="float: right; margin-top: 8px;">
+                                    <i class="fa fa-magic fa-2x" style="background-color: orange;
+                                    border-radius: 18px; border: 7px; border-color: orange; border-style: solid; cursor: pointer;" aria-hidden="true">
+                                    </i>
+                                </div>`);
     const COMPARE_BUTTON = `<li class="inaktiv">
-                                <a rel="nofollow" target="_blank" href="http://localhost:3000"><strong>Összehasonlítás(<span class="compare-number"></span>)</strong></a>
-                            </li>`
+                                <a rel="nofollow" target="_blank"
+                                    href="http://localhost:3000"
+                                    id="compareButton">
+                                    <strong>Összehasonlítás(<span class="compare-number">0</span>)</strong>
+                                </a>
+                            </li>`;
+    const ADD_FOR_COMPARE_BUTTON = $(`<input type="button" style="float: right; margin-top: 15px; margin-right: 5px; background-color: #f0f0f0;
+                                        border: 1px solid #e6e6e6;
+                                        padding: 3px 10px;
+                                        font-size: 12px;
+                                        color: #0464a4;
+                                        cursor: pointer;" value="Összehasonlít">`);
 
-    let lastClickedButton;
+    let lastClickedGetValueButton;
 
+    //EVENT HANDLERS
     let getValueButtonClicked = (event) => {
-        lastClickedButton = event.target;
-        const carRef = $(lastClickedButton).parentsUntil('.talalati_lista').prev().find('h2>a')[0].href;
+        lastClickedGetValueButton = event.target;
+        const carRef = getUrlForButton(lastClickedGetValueButton);
         const data = { "carUrls": [carRef] };
-        refreshUrlList(carRef);
         const value = $.ajax({
                 url: 'https://localhost:5000',
                 method: 'POST',
                 contentType: "application/json;",
                 data: JSON.stringify(data)
         }).done(function( response ) {
-          if(lastClickedButton && response && response[0].worth) {
+          if(lastClickedGetValueButton && response && response[0].worth) {
             const worthElement = "<div style='float: right; margin-top: 15px; font-weight: bold;'>ÉRTÉK: " + response[0].worth + "</div>";
             replaceElement($('.fa-spinner')[0], worthElement);
           }
         });
-        replaceElement(lastClickedButton, SPINNER);
+        replaceElement(lastClickedGetValueButton, SPINNER);
+    };
+
+    let addForCompareClicked = (event) => {
+        const carRef = $(event.target).parentsUntil('.talalati_lista').prev().find('h2>a')[0].href;
+        refreshUrlList(carRef);
     };
     
-    function refreshUrlList(ref) {
-        urlList.push(ref);
-        $('.compare-number').html(urlList.length);
-    }
+    let saveCarRefs = (event) => {
+        localStorage.setItem('carUrls', urlList);
+    };
 
+    //BIND EVENTS
     GET_VALUE_BUTTON.click(getValueButtonClicked);
+    ADD_FOR_COMPARE_BUTTON.click(addForCompareClicked);
+    $("#compareButton").click(saveCarRefs);
 
     //add bootstrap to head
     $('head').append('<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">');
@@ -55,12 +74,15 @@
     //ready
     $(() => {
         if(document.URL.match(/https:\/\/(www|new){1}.hasznaltauto.hu\/talalatilista\/auto.*/g)) {
-            $('.talalati_lista_vetelar').after(GET_VALUE_BUTTON);
+            const buttons = $('<div></div>').append(GET_VALUE_BUTTON).append(ADD_FOR_COMPARE_BUTTON);
+            $('.talalati_lista_vetelar').after(buttons);
             $('.tabmenu').children().last().after(COMPARE_BUTTON);
-        }        
+        }
     });
 
-    //DOM manipulation utilities
+    //////////////////////////////
+    //DOM manipulation utilities//
+    //////////////////////////////
 
     //Replaces the element with new element. Uses the previousElementSibling attribute, so element must have it
     function replaceElement(element, newElement) {
@@ -70,6 +92,20 @@
         }
         $(element.previousElementSibling).after(newElement);
         $(element).remove();
+    }
+
+    function refreshUrlList(ref) {
+        if(!urlList.includes(ref)) {
+            urlList.push(ref);   
+        } else {
+            const index = urlList.indexOf(ref);
+            urlList.splice(index, index + 1);
+        }
+        $('.compare-number').html(urlList.length);
+    }
+
+    function getUrlForButton(button) {
+        return $(button).parentsUntil('.talalati_lista').prev().find('h2>a')[0].href;
     }
 })();
 
