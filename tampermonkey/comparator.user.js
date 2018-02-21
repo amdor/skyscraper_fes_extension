de3c37edcdc19663b1dc35a7c3bd8a31eca22a03
Zsolt <lordmairtis@gmail.com>
2018-02-21 22:05:09 +0100

    timeout on worth request and sending html content along

--- tampermonkey/comparator.user.js
+++ tampermonkey/comparator.user.js
@@ -56,39 +56,52 @@ const ADD_FOR_COMPARE_BUTTON = $(`<input type="button" style="float: right; marg
                                     color: #0464a4;
                                     cursor: pointer;" value="Összehasonlít">`);
 ////START EVENT_HANDLERS.JS
+
 //EVENT HANDLERS
 function getValueButtonClicked(event) {
-    lastClickedGetValueButton = event.target;
-    const carRef = getUrlForButton(lastClickedGetValueButton);
-    const data = { "carUrls": [carRef] };
-    const value = $.ajax({
-            url: 'https://localhost:5000',
-            method: 'POST',
-            contentType: "application/json;",
-            data: JSON.stringify(data)
-    }).done(function( response ) {
-      if(lastClickedGetValueButton && response && response[0].worth) {
-        const worthElement = "<div style='float: right; margin-top: 15px; font-weight: bold;'>ÉRTÉK: " + response[0].worth + "</div>";
-        replaceElement($('.fa-spinner')[0], worthElement);
-      }
-    });
-    replaceElement(lastClickedGetValueButton, SPINNER);
+		lastClickedGetValueButton = event.target;
+		const carRef = getUrlForButton(lastClickedGetValueButton);
+		$.ajax({
+			url: carRef,
+			success: (htmlContent) => {
+				const data = { "carUrls": [carRef], 'html': htmlContent };
+				$.ajax({
+						url: 'https://localhost:5000',
+						method: 'POST',
+						contentType: "application/json;",
+						data: JSON.stringify(data),
+						timeout: 3000,
+						success: ( response ) => {
+							if(lastClickedGetValueButton && response && response[0].worth) {
+								const worthElement = "<div style='float: right; margin-top: 15px; font-weight: bold;'>ÉRTÉK: " + response[0].worth + "</div>";
+								replaceElement('.fa-spinner', worthElement);
+							}
+						},
+						error: (jqXHR, textStatus) => {
+							console.error(jqXHR.status + textStatus);
+							replaceElement('.fa-spinner', GET_VALUE_BUTTON);
+						}
+				});
+			}
+		});
+
+		replaceElement(lastClickedGetValueButton, SPINNER);
 }
 
 function addForCompareClicked(event) {
-    const carRef = getUrlForButton(event.target);
-    //switch color and background color
-    const backgroundColor = $(event.target).css('background-color');
-    const color = $(event.target).css('color');
-    $(event.target).css('background-color', color);
-    $(event.target).css('color', backgroundColor);    
+		const carRef = getUrlForButton(event.target);
+		//switch color and background color
+		const backgroundColor = $(event.target).css('background-color');
+		const color = $(event.target).css('color');
+		$(event.target).css('background-color', color);
+		$(event.target).css('color', backgroundColor);
 
-    refreshUrlList(carRef);
-    sessionStorage.setItem('carUrls', JSON.stringify(urlList));
+		refreshUrlList(carRef);
+		sessionStorage.setItem('carUrls', JSON.stringify(urlList));
 }
 
 function saveCarRefs (event) {
-    GM_setValue('carUrls', JSON.stringify(urlList));
+		GM_setValue('carUrls', JSON.stringify(urlList));
 }
 
 //BIND EVENTS
