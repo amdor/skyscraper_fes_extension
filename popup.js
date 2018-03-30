81839bba4de26efee05489169ffe65611e1af48a
Zsolt <lordmairtis@gmail.com>
2018-03-30 20:50:24 +0200

    Chrome extension, add and remove urls to the extension popup

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
+// Copyright (c) 2018 Zsolt Deák. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
 // found in the LICENSE file.
+
 
 /**
  * Get the current URL.
@@ -9,90 +10,80 @@
  *   is found.
  */
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+	// Query filter to be passed to chrome.tabs.query - see
+	// https://developer.chrome.com/extensions/tabs#method-query
+	var queryInfo = {
+		active: true,
+		currentWindow: true
+	};
 
-  chrome.tabs.query(queryInfo, (tabs) => {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
+	chrome.tabs.query(queryInfo, (tabs) => {
+		// chrome.tabs.query invokes the callback with a list of tabs that match the
+		// query. When the popup is opened, there is certainly a window and at least
+		// one tab, so we can safely assume that |tabs| is a non-empty array.
+		// A window can only have one active tab at a time, so the array consists of
+		// exactly one tab.
+		var tab = tabs[0];
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
+	// A tab is a plain object that provides information about the tab.
+	// See https://developer.chrome.com/extensions/tabs#type-Tab
+	var url = tab.url;
 
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
+	// tab.url is only available if the "activeTab" permission is declared.
+	// If you want to see the URL of other tabs (e.g. after removing active:true
+	// from |queryInfo|), then the "tabs" permission is required to see their
+	// "url" properties.
+	console.assert(typeof url == 'string', 'tab.url should be a string');
 
-    callback(url);
-  });
+		callback(url);
+	});
 
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, (tabs) => {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+	// Most methods of the Chrome extension APIs are asynchronous. This means that
+	// you CANNOT do something like this:
+	//
+	// var url;
+	// chrome.tabs.query(queryInfo, (tabs) => {
+	//   url = tabs[0].url;
+	// });
+	// alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
-/**
- * Change the background color of the current page.
- *
- * @param {string} color The new background color.
- */
-function changeBackgroundColor(color) {
-  var script = 'document.body.style.backgroundColor="' + color + '";';
-  // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-  // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-  // into a page. Since we omit the optional first argument "tabId", the script
-  // is inserted into the active tab of the current window, which serves as the
-  // default.
-  chrome.tabs.executeScript({
-    code: script
-  });
+
+function showSavedCarLinks() {
+	chrome.storage.sync.get(null, (cars) => {
+		if(chrome.runtime.lastError) {
+			console.error(chrome.runtime.lastError);
+		}
+		addUrlsToList(Object.keys(cars) || [])
+	});
 }
 
-/**
- * Gets the saved background color for url.
- *
- * @param {string} url URL whose background color is to be retrieved.
- * @param {function(string)} callback called with the saved background color for
- *     the given url on success, or a falsy value if no color is retrieved.
- */
-function getSavedBackgroundColor(url, callback) {
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-  // for chrome.runtime.lastError to ensure correctness even when the API call
-  // fails.
-  chrome.storage.sync.get(url, (items) => {
-    callback(chrome.runtime.lastError ? null : items[url]);
-  });
+
+function saveCarLink(url, callback) {
+	chrome.storage.sync.get(null, (cars) => {
+		if(chrome.runtime.lastError) {
+			console.error(chrome.runtime.lastError);
+			callback(false);
+		}
+		const urls = Object.keys(cars);
+		if(urls.length < 10 && !urls.includes(url)) {
+			cars[url] = '';
+			chrome.storage.sync.set(cars);
+			callback(true);
+		}
+		callback(false);
+	});
 }
 
-/**
- * Sets the given background color for url.
- *
- * @param {string} url URL for which background color is to be saved.
- * @param {string} color The background color to be saved.
- */
-function saveBackgroundColor(url, color) {
-  var items = {};
-  items[url] = color;
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-  // optional callback since we don't need to perform any action once the
-  // background color is saved.
-  chrome.storage.sync.set(items);
+function addUrlsToList(urls) {
+	for(url of urls) {
+		const listItem = $('<li><i class="fa fa-minus fa-3x"></i> ' + url + '</li>');
+		$('#urlList').append(listItem);
+		listItem.click(() => {
+			listItem.remove();
+			chrome.storage.sync.remove(url);
+		})
+	}
 }
 
 // This extension loads the saved background color for the current tab if one
@@ -104,23 +95,16 @@ function saveBackgroundColor(url, color) {
 // chrome.storage.local allows the extension data to be synced across multiple
 // user devices.
 document.addEventListener('DOMContentLoaded', () => {
-  getCurrentTabUrl((url) => {
-    var dropdown = document.getElementById('dropdown');
+	var addCarButton = $('#addCar');
+	addCarButton.click(() => {
+		getCurrentTabUrl((url) => {
+			saveCarLink(url, (success) => {
+				if(success) {
+					addUrlsToList([url])
+				}
+			});
+		});
+	});
 
-    // Load the saved background color for this page and modify the dropdown
-    // value, if needed.
-    getSavedBackgroundColor(url, (savedColor) => {
-      if (savedColor) {
-        changeBackgroundColor(savedColor);
-        dropdown.value = savedColor;
-      }
-    });
-
-    // Ensure the background color is changed and saved when the dropdown
-    // selection changes.
-    dropdown.addEventListener('change', () => {
-      changeBackgroundColor(dropdown.value);
-      saveBackgroundColor(url, dropdown.value);
-    });
-  });
+	showSavedCarLinks();
 });
